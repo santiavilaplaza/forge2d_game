253020ec991352789277fdc185054d3c12b00ebc
santiavilaplaza <santiagoavilaplaza@gmail.com>
2025-03-12 18:39:59 +0100

    Step 3: Add image

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
+import 'components/game.dart';
+
 void main() {
   runApp(
     GameWidget.controlled(
-      gameFactory: FlameGame.new,
+      gameFactory: MyPhysicsGame.new,
     ),
   );
 }
